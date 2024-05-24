070e6ff09b9df5b2a7c40de2e310de8277ab5645
lcam15 <lcameo15@gmail.com>
2024-05-23 20:15:23 -0400

    c# code

---
+++ html/51/Shirts.cs
@@ -0,0 +1,21 @@
+﻿namespace Hw51Shirts {
+    internal class Shirts {
+        static void Main(string[] args)
+        {
+            Shirt();
+        }
+        static void Shirt()
+        {
+            string[] colors = { "red", "blue", "green" };
+            string[] patterns = { "checkered", "stripes", "plain"};
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                for (int j = 0; j < patterns.Length; j++)
+                {
+                    Console.WriteLine(colors[i]+":"+ patterns[j]);
+                }
+            }
+        }
+    }
+}
